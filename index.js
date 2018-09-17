58b7276ccb9fc35d98c441bf0189c82a1dbc3144
Kevin Deisz <kevin.deisz@gmail.com>
2018-09-17 13:05:06 -0400

    Commit distribution

---
+++ index.js
@@ -0,0 +1,199 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.TinyRouter = exports.Link = exports.withRouter = exports.History = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var _React$createContext = _react.default.createContext({
+  currentPath: "",
+  onLinkClick: function onLinkClick() {},
+  onPathChange: function onPathChange() {},
+  onPathReplace: function onPathReplace() {}
+}),
+    Provider = _React$createContext.Provider,
+    Consumer = _React$createContext.Consumer;
+
+var History =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(History, _Component);
+
+  function History() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, History);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(History)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handleLinkClick", function (event) {
+      event.preventDefault();
+
+      _this.handlePathChange(event.currentTarget.pathname);
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handlePathChange", function (nextPath) {
+      _this.setState({
+        currentPath: nextPath
+      });
+
+      window.history.pushState({}, "", nextPath);
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handlePathReplace", function (nextPath) {
+      _this.setState({
+        currentPath: nextPath
+      });
+
+      window.history.replaceState({}, "", nextPath);
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "state", {
+      currentPath: window.location.pathname,
+      onLinkClick: _this.handleLinkClick,
+      onPathChange: _this.handlePathChange,
+      onPathReplace: _this.handlePathReplace
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handlePopState", function (_ref) {
+      var location = _ref.currentTarget.location;
+
+      _this.setState({
+        currentPath: location.pathname
+      });
+    });
+
+    return _this;
+  }
+
+  _createClass(History, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      window.addEventListener("popstate", this.handlePopState);
+    }
+  }, {
+    key: "componentWillUnmount",
+    value: function componentWillUnmount() {
+      window.removeEventListener("popstate", this.handlePopState);
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var children = this.props.children;
+      return _react.default.createElement(Provider, {
+        value: this.state
+      }, children);
+    }
+  }]);
+
+  return History;
+}(_react.Component);
+
+exports.History = History;
+
+var withRouter = function withRouter(Child) {
+  var Parent = function Parent(props) {
+    return _react.default.createElement(Consumer, null, function (state) {
+      return _react.default.createElement(Child, _extends({}, state, props));
+    });
+  };
+
+  var childName = Child.displayName || Child.name || "Component";
+  Parent.displayName = "withRouter(".concat(childName, ")");
+  return Parent;
+};
+
+exports.withRouter = withRouter;
+
+var Link = function Link(_ref2) {
+  var to = _ref2.to,
+      children = _ref2.children,
+      props = _objectWithoutProperties(_ref2, ["to", "children"]);
+
+  return _react.default.createElement(Consumer, null, function (_ref3) {
+    var onLinkClick = _ref3.onLinkClick;
+    return _react.default.createElement("a", _extends({}, props, {
+      href: to,
+      onClick: onLinkClick
+    }), children);
+  });
+};
+
+exports.Link = Link;
+
+var match = function match(ast, currentPath) {
+  var segments = currentPath.split("/").filter(Boolean);
+  var dynamics = [];
+  var currentAst = ast;
+  var matched = true;
+
+  for (var idx = 0; idx < segments.length; idx += 1) {
+    if (currentAst.next[segments[idx]]) {
+      currentAst = currentAst.next[segments[idx]];
+    } else if (currentAst.next[":dynamic"]) {
+      dynamics.push(segments[idx]);
+      currentAst = currentAst.next[":dynamic"];
+    } else {
+      matched = false;
+      break;
+    }
+  }
+
+  matched = matched && currentAst.render;
+
+  if (!matched && ast.default) {
+    return ast.default();
+  }
+
+  if (!matched) {
+    return null;
+  }
+
+  return currentAst.render.apply(null, dynamics);
+};
+
+var TinyRouter = function TinyRouter(_ref4) {
+  var ast = _ref4.ast;
+  return _react.default.createElement(Consumer, null, function (_ref5) {
+    var currentPath = _ref5.currentPath;
+    return match(ast, currentPath);
+  });
+};
+
+exports.TinyRouter = TinyRouter;
---
+++ router.macro.js
@@ -0,0 +1,149 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+var _require = require("babel-plugin-macros"),
+    createMacro = _require.createMacro;
+
+var buildTinyRouterOpening = function buildTinyRouterOpening(t, routerIdent, ast) {
+  return t.jsxOpeningElement(routerIdent, [t.jsxAttribute(t.jsxIdentifier("ast"), t.jsxExpressionContainer(ast))], true);
+};
+
+var buildTinyRouter = function buildTinyRouter(t, routerIdent, ast) {
+  return t.jsxElement(buildTinyRouterOpening(t, routerIdent, ast), null, [], true);
+};
+
+var buildDynamicProp = function buildDynamicProp(t, prop) {
+  return t.jsxAttribute(t.jsxIdentifier(prop), t.jsxExpressionContainer(t.identifier(prop)));
+};
+
+var buildRenderFunc = function buildRenderFunc(t, props, rendered) {
+  return t.functionExpression(null, props.map(function (prop) {
+    return t.identifier(prop);
+  }), t.blockStatement([t.returnStatement(rendered)]));
+};
+
+var buildASTExpression = function buildASTExpression(t, ast) {
+  var properties = Object.keys(ast).map(function (key) {
+    var value = ast[key];
+
+    if (!value.type) {
+      switch (_typeof(value)) {
+        case "object":
+          value = buildASTExpression(t, value);
+          break;
+
+        case "number":
+          value = t.numericLiteral(value);
+          break;
+
+        case "string":
+          value = t.stringLiteral(value);
+          break;
+      }
+    }
+
+    var objectKey = key.startsWith(":") ? t.stringLiteral(key) : t.identifier(key);
+    return t.objectProperty(objectKey, value);
+  });
+  return t.objectExpression(properties);
+};
+
+var extractProp = function extractProp(element, prop) {
+  var attributes = element.openingElement.attributes;
+  var index = attributes.findIndex(function (_ref) {
+    var name = _ref.name.name;
+    return name === prop;
+  });
+
+  if (index !== -1) {
+    var value = attributes.splice(index, 1)[0].value;
+    return value && value.value;
+  }
+
+  return null;
+};
+
+var addToAST = function addToAST(t, routingAST, childNode) {
+  var path = extractProp(childNode, "path");
+  var segments = path.split("/").filter(Boolean);
+  var currentTree = routingAST;
+  var props = [];
+  segments.forEach(function (segment) {
+    var normalized = segment;
+
+    if (normalized.startsWith(":")) {
+      props.push(normalized.slice(1));
+      normalized = ":dynamic";
+    }
+
+    if (!currentTree.next.hasOwnProperty(normalized)) {
+      currentTree.next[normalized] = {
+        next: {}
+      };
+    }
+
+    currentTree = currentTree.next[normalized];
+  });
+
+  if (currentTree.hasOwnProperty("render")) {
+    throw new Error("".concat(path, " has an overlapping route"));
+  }
+
+  props.forEach(function (prop) {
+    return childNode.openingElement.attributes.push(buildDynamicProp(t, prop));
+  });
+  currentTree.render = buildRenderFunc(t, props, childNode);
+};
+
+var buildAST = function buildAST(t, declaration) {
+  var ast = {
+    next: {}
+  };
+  var children = declaration.node.children.filter(function (_ref2) {
+    var type = _ref2.type;
+    return type === "JSXElement";
+  });
+  var defaultIndex = children.findIndex(function (node) {
+    return node.openingElement.attributes.some(function (attribute) {
+      return attribute.name.name === "default";
+    });
+  });
+
+  if (defaultIndex !== -1) {
+    var defaultChild = children.splice(defaultIndex, 1)[0];
+    extractProp(defaultChild, "default");
+    ast.default = buildRenderFunc(t, [], defaultChild);
+  }
+
+  children.forEach(function (node) {
+    return addToAST(t, ast, node);
+  });
+  return buildASTExpression(t, ast);
+};
+
+var routerMacro = function routerMacro(_ref3) {
+  var references = _ref3.references,
+      t = _ref3.babel.types;
+  var _references$default = references.default,
+      routers = _references$default === void 0 ? [] : _references$default;
+  var index = 0;
+  var TinyRouter = routers[0].scope.getProgramParent().path.scope.bindings.TinyRouter;
+  routers.forEach(function (router) {
+    if (router.parent.type !== "JSXOpeningElement") {
+      return;
+    }
+
+    TinyRouter.referenced = true;
+    TinyRouter.references += 1;
+    var declaration = router.parentPath.parentPath;
+    var ast = buildAST(t, declaration);
+    var routerIdent = Object.assign({}, TinyRouter.identifier, {
+      type: "JSXIdentifier"
+    });
+    declaration.replaceWith(buildTinyRouter(t, routerIdent, ast));
+    index += 1;
+  });
+};
+
+module.exports = createMacro(routerMacro);
