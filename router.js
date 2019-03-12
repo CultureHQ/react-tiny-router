ad8c661142658efd29cf2daee39234a0c186085d
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-12 14:21:19 -0400

    Bump dependencies

--- router.js
+++ router.js
@@ -27,11 +27,11 @@ function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) ===
 
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 var _React$createContext = _react.default.createContext({
   currentPath: "",
@@ -79,7 +79,7 @@ function (_Component) {
         onPathChange(event.currentTarget.pathname);
       }
     };
-    _this.handlePopState = _this.handlePopState.bind(_assertThisInitialized(_assertThisInitialized(_this)));
+    _this.handlePopState = _this.handlePopState.bind(_assertThisInitialized(_this));
     return _this;
   }
 
